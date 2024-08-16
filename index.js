1c9de27dd3de7aa301eb6d10bd05a0757bb2061b
Ryan Montgomery <coap.ryanmontgomery@gmail.com>
2024-08-15 18:04:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 function addingEventListener() {
     const input = document.getElementById('button');
-  
+
     function clickAlert() {
       alert('I was clicked!');
     }
-  
+
     input.addEventListener('click', clickAlert);
   }
-  
+
   // Call the function to execute addEventListener and activate the event listener
   addingEventListener();
+
+
   
